a6e44ab4a26cd23e3c09aee862da5d7147ed6531
Jooah Seo <seo.jooah@gmail.com>
2020-12-20 19:05:02 -0500

    changed bg to antiquewhite

---
+++ todo.js
@@ -0,0 +1,39 @@
+const btn = document.querySelector('#submitBtn');
+//const formBox = document.querySelector('#newTodo');
+const inputBox = document.getElementById("newTodo");
+const body = document.querySelector('body');
+const list = document.querySelector('#list');
+
+
+let storedValues = window.localStorage.myitems;
+if(storedValues){
+    list.innerHTML = storedValues;
+}
+
+
+btn.addEventListener('click',function(e){
+    e.preventDefault();
+    const newLi = document.createElement('li');
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add("btn"); //for css selection
+    removeBtn.innerText = "Remove";
+    newLi.innerText = inputBox.value;
+
+    newLi.append(removeBtn);
+    list.append(newLi);
+    store();
+    inputBox.value ="";
+})
+
+list.addEventListener('click',function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("done");
+    }else if(e.target.tagName === "BUTTON"){
+        e.target.parentElement.remove();
+    }
+    store();
+})
+
+function store(){
+    window.localStorage.myitems = list.innerHTML;
+}
